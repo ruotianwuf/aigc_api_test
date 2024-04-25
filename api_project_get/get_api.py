d73daaa2e4d65ed561a3019f9ebf30af1b0bd2d8
liaojiongyi <liaojiongyi@gmail.com>
2024-04-26 03:04:20 +0800

    api调用

---
+++ api_project_get/get_api.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+import uuid
+import time
+import requests
+import app
+from api_project_get.auth_util import gen_sign_headers
+
+def sync_vivogpt(prompt):
+
+    APP_ID = '3032660331'
+    APP_KEY = 'LxpYKtKbgakYmMTN'
+    URI = '/vivogpt/completions'
+    DOMAIN = 'api-ai.vivo.com.cn'
+    METHOD = 'POST'
+
+    params = {
+        'requestId': str(uuid.uuid4())
+    }
+    print('requestId:', params['requestId'])
+
+    data = {
+        'prompt': prompt,
+        'model': 'vivo-BlueLM-TB',
+        'sessionId': str(uuid.uuid4()),
+        'extra': {
+            'temperature': 0.9
+        }
+    }
+    headers = gen_sign_headers(APP_ID, APP_KEY, METHOD, URI, params)
+    headers['Content-Type'] = 'application/json'
+
+    start_time = time.time()
+    url = 'https://{}{}'.format(DOMAIN, URI)
+    response = requests.post(url, json=data, headers=headers, params=params)
+
+    if response.status_code == 200:
+        res_obj = response.json()
+        print(f'response:{res_obj}')
+        if res_obj['code'] == 0 and res_obj.get('data'):
+            content = res_obj['data']['content']
+            print(f'final content:\n{content}')
+    else:
+        print(response.status_code, response.text)
+    end_time = time.time()
+    timecost = end_time - start_time
+    print('请求耗时: %.2f秒' % timecost)
+
+    return content
+
+
